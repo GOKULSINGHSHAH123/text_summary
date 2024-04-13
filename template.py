cb7d5a17c27ff9c4d07beab6b2a793c32d436b4e
GOKULSINGHSHAH123 <148846692+GOKULSINGHSHAH123@users.noreply.github.com>
2024-04-13 08:46:35 +0530

    Changes in template

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "text_summary"
+project_name = "text_summarizer"
 
 list_of_files = [
     "text_summary/github/workflows/.gitkeep",
